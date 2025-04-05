8091128e0fffc0ebca22b3555fe55e141a4d481a
João Sérgio Viegas <joao.s.viegas@tecnico.ulisboa.pt>
2025-04-05 13:07:54 +0100

    Added Rolling block h2

--- src/components/RollingBlock/RollingBlock.jsx
+++ src/components/RollingBlock/RollingBlock.jsx
@@ -3,18 +3,27 @@ import './RollingBlock.css';
 
 const RollingBlock = ({ data }) => {
   return (
-    <div className="slider">
-      <div className="slide-track">
-        {data.map((item, index) => (
-          <div className="slide" key={index}>
-            <img src={item.imageUrl} alt={item.name || ''} />
-          </div>
-        ))}
-        {data.slice(0, 7).map((item, index) => (
-          <div className="slide" key={`repeat-${index}`}>
-            <img src={item.imageUrl} alt={item.name || ''} />
-          </div>
-        ))}
+    <div className="rolling-block">
+
+      <div className="rolling-block-header">
+        <div className="rolling-block-title">
+          <h2>Com a presença de</h2>
+        </div>
+      </div>
+
+      <div className="slider">
+        <div className="slide-track">
+          {data.map((item, index) => (
+            <div className="slide" key={index}>
+              <img src={item.imageUrl} alt={item.name || ''} />
+            </div>
+          ))}
+          {data.slice(0, 7).map((item, index) => (
+            <div className="slide" key={`repeat-${index}`}>
+              <img src={item.imageUrl} alt={item.name || ''} />
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
